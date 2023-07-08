809f499e2cf5b8b7f2077c18f6d32baed5941573
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-09 00:25:34 +0700

    commit-get-user-by-id-#23

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
@@ -32,8 +32,13 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) 
+      return `not found user`;
+
+    return this.userModel.findOne({
+        id: id
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
