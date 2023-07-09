212eb93f3f624c88853c2a114a585f372af01c41
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-09 12:16:35 +0700

    commit-delete-a-user-#25

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends OmitType(CreateUserDto, ['password'] as const) {
-    _id: string;
+    _id: string; 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -45,7 +45,12 @@ export class UsersService {
     return await this.userModel.updateOne({ _id: updateUserDto._id }, {...updateUserDto});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) 
+      return `not found user`;
+
+    return this.userModel.deleteOne({
+      _id: id
+    });
   }
 }
