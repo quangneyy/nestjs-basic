b163067d70b984a7bc146fd8a7670f026e5159e2
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-10 18:06:10 +0700

    commit-nestjs-guard-#38

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TestGuard } from './test.guard';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,14 +10,12 @@ export class UsersController {
 
   @Post()
   create(
-    // @Body("email") email: string,
-    // @Body("password") password: string,
-    // @Body("name") name: string,
     @Body( ) createUserDto: CreateUserDto
     ) {
     return this.usersService.create(createUserDto);
   }
 
+  // @UseGuards(TestGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
