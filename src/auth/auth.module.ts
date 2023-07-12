cac1d469cef61b4da9fe755288c8f80fe7b3ffb9
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-12 13:43:51 +0700

    fix-bug-#43

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,15 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.stratrgy';
 import { JwtModule } from '@nestjs/jwt';
+import ms from 'ms';
 @Module({
   imports: [UsersModule, PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-            expiresIn: configService.get<string>('JWT_EXPIRE'),
+            expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRE')),
         },
       }),
       inject: [ConfigService],
--- src/auth/passport/local.stratrgy.ts
+++ src/auth/passport/local.stratrgy.ts
@@ -14,6 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException();
     }
-    return user;
+    return user; // req.user
   }
 }
