0e356bf226c3ca275b566fb6784401093a9d265f
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-14 15:58:13 +0700

    update-generate-modules-company-#53

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -2,17 +2,17 @@ import { IsNotEmpty } from "class-validator";
 
 export class CreateCompanyDto {
     @IsNotEmpty({
-        message: "Email không được để trống",
+        message: "Name không được để trống",
     })
     name: string;
 
     @IsNotEmpty({
-        message: "Password không được để trống",
+        message: "Address không được để trống",
     })
     address: string;
 
     @IsNotEmpty({
-        message: "Password không được để trống",
+        message: "Description không được để trống",
     })
     description: string;
 }
--- src/companies/schemas/company.schema.ts
+++ src/companies/schemas/company.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type CompanyDocument = HydratedDocument<Company>;
 
@@ -16,19 +16,19 @@ export class Company {
 
   @Prop({ type: Object })
   createdBy: {
-    _id: string;
+    _id: mongoose.Schema.Types.ObjectId;
     email: string;
   }
 
   @Prop({ type: Object })
   updatedBy: {
-    _id: string;
+    _id: mongoose.Schema.Types.ObjectId;
     email: string;
   }
 
   @Prop({ type: Object })
   deletedBy: {
-    _id: string;
+    _id: mongoose.Schema.Types.ObjectId;
     email: string;
   }
 
