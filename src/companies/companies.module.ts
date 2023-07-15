5725af8c33d1eb5091a42c353c772404b5414468
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-15 16:54:52 +0700

    create-company-#54

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,8 +1,11 @@
+import { Company, CompanySchema } from './schemas/company.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }])],
   controllers: [CompaniesController],
   providers: [CompaniesService]
 })
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,11 +1,20 @@
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Company, CompanyDocument } from './schemas/company.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Injectable()
 export class CompaniesService {
+
+  constructor(
+    @InjectModel(Company.name)
+    private companyModel: SoftDeleteModel<CompanyDocument>
+  ) { }
+
   create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+    return this.companyModel.create({ ...createCompanyDto })
   }
 
   findAll() {
