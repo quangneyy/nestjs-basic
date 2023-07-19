a85f8deed833b0e1f278371ab2eca724d2c8a659
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-20 00:16:52 +0700

    query-with-pagination-2-#59.2

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -26,7 +26,7 @@ export class CompaniesService {
   }
 
   async findAll(currentPage: number, limit: number, qs: string) {
-    const { filter, sort, projection, population } = aqp(qs);
+    const { filter, sort, population } = aqp(qs);
     delete filter.page;
     delete filter.limit;
 
@@ -39,8 +39,7 @@ export class CompaniesService {
     const result = await this.companyModel.find(filter)
       .skip(offset)
       .limit(defaultLimit)
-      // @ts-ignore: Unreachable code error
-      .sort(sort)
+      .sort(sort as any)
       .populate(population)
       .exec();
 
