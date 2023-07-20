3fb520c041d533aa1235ec0edbc3cb62040de61f
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-07-20 17:05:20 +0700

    version-apis-#62

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
@@ -27,6 +27,14 @@ async function bootstrap() {
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
   });
+
+  // config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'] //v1, v2
+  });
+
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
