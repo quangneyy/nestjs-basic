dd5294229480568b56dc64491869bb76017ec7a2
quangneyy <quangney12602@gmail.com>
2023-08-02 11:48:35 +0700

    job-CRUD-#72

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 import { CompaniesModule } from './companies/companies.module';
+import { JobModule } from './job/job.module';
 
 @Module({
   imports: [
@@ -28,7 +29,8 @@ import { CompaniesModule } from './companies/companies.module';
 
     UsersModule,
     AuthModule,
-    CompaniesModule
+    CompaniesModule,
+    JobModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -27,9 +27,10 @@ async function bootstrap() {
 
   // config cors
   app.enableCors({
-    "origin": "*",
+    "origin": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
+    credentials: true
   });
 
   // config versioning
