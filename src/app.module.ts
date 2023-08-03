9a6604329af875d11fc7b0dceb09ab426021a5c4
quangneyy <quangney12602@gmail.com>
2023-08-03 12:38:56 +0700

    upload-files-first-#75

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 import { CompaniesModule } from './companies/companies.module';
 import { JobModule } from './job/job.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -30,7 +31,8 @@ import { JobModule } from './job/job.module';
     UsersModule,
     AuthModule,
     CompaniesModule,
-    JobModule
+    JobModule,
+    FilesModule
   ],
   controllers: [AppController],
   providers: [AppService],
