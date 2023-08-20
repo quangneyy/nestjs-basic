4354946ca6ccac3e6cc22b76bc4ddbcb07709422
quangneyy <quangney12602@gmail.com>
2023-08-21 00:22:18 +0700

    crud-permissions-#86

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,26 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import aqp from 'api-query-params';
+import mongoose from 'mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { PermissionDocument } from './schemas/permission.schema';
 
 @Injectable()
+
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectModel(Permissions.name)
+    private permissionModel: SoftDeleteModel<PermissionDocument>
+  ) { }
+
+  async create(createPermissionDto: CreatePermissionDto, user: IUser) {
+    const { name, apiPath, method, module } = createPermissionDto;
+
+    const isExist = await this.permissionModel.findOne({ apiPath, method });
+    if (isExist) {
+      throw new BadRequestException(`Permission với apiPath=${apiPath}, method=${method} đã tồn tại!`)
+    }
+
+    const newPermission = await this.permissionModel.create({
+      name, apiPath, method, module,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+
+    return {
+      _id: newPermission?._id,
+      createAt: newPermission?.createdAt
+    };
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population, projection } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.permissionModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.permissionModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .select(projection as any)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, // trang hien tai
+        pageSize: limit, // So luong ban ghi da lay
+        pages: totalPages, // tong so trang voi dieu kien query
+        total: totalItems // tong so phan tu (so ban ghi)
+      },
+      result // ket qua query
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException("not found permission")
+    }
+
+    return await this.permissionModel.findById(id);
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
+  async update(_id: string, updatePermissionDto: UpdatePermissionDto, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw new BadRequestException("not found permission")
+    }
+    const { module, method, apiPath, name } = updatePermissionDto;
+
+    const updated = await this.permissionModel.updateOne(
+      { _id },
+      {
+        module, method, apiPath, name,
+        updatedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      }
+    );
+    return updated;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
+  async remove(id: string, user: IUser) {
+    await this.permissionModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      }
+    )
+    return this.permissionModel.softDelete({
+      _id: id,
+    })
   }
 }
--- src/resumes/dto/create-resume.dto.ts
+++ src/resumes/dto/create-resume.dto.ts
@@ -26,3 +26,16 @@ export class CreateResumeDto {
     @IsNotEmpty({ message: 'jobId không được để trống', })
     jobId: mongoose.Schema.Types.ObjectId;
 }
+
+export class CreateUserCvDto {
+    @IsNotEmpty({ message: 'url không được để trống', })
+    url: string;
+
+    @IsNotEmpty({ message: 'companyId không được để trống', })
+    @IsMongoId({ message: 'companyId is a mongo id' })
+    companyId: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({ message: 'jobId không được để trống', })
+    @IsMongoId({ message: 'jobId is a mongo id' })
+    jobId: mongoose.Schema.Types.ObjectId;
+}
