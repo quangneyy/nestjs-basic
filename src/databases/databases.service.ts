7eb7e3695842280a1c937dff5c627289cd059f65
quangneyy <quangney12602@gmail.com>
2023-08-30 16:06:16 +0700

    fake-data#92

--- src/databases/databases.service.ts
+++ src/databases/databases.service.ts
@@ -1,5 +1,6 @@
+import { INIT_PERMISSIONS, ADMIN_ROLE, USER_ROLE } from './sample';
 import { Permission, PermissionDocument } from 'src/permissions/schemas/permission.schema';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { User, UserDocument } from 'src/users/schemas/user.schema';
@@ -9,6 +10,7 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DatabasesService implements OnModuleInit {
+    private readonly logger = new Logger(DatabasesService.name);
 
     constructor(
         @InjectModel(User.name)
@@ -24,7 +26,76 @@ export class DatabasesService implements OnModuleInit {
         private userService: UsersService
     ) { }
 
-    onModuleInit() {
-        console.log(`The module has been initialized.`);
+    async onModuleInit() {
+        const inInit = this.configService.get<string>("SHOULD_INIT");
+        if (Boolean(inInit)) {
+
+            const countUser = await this.userModel.count({});
+            const countPermission = await this.permissionModel.count({});
+            const countRole = await this.roleModel.count({});
+
+            //create permission
+            if (countPermission === 0) {
+                await this.permissionModel.insertMany(INIT_PERMISSIONS);
+                //bulk create
+            }
+
+            // create role 
+            if (countRole === 0) {
+                const permissions = await this.permissionModel.find({}).select("_id");
+                await this.roleModel.insertMany([
+                    {
+                        name: ADMIN_ROLE,
+                        description: "Admin thì full quyền :v",
+                        isActive: true,
+                        permissions: permissions
+                    },
+                    {
+                        name: USER_ROLE,
+                        description: "Người dùng/Ứng viên sử dụng hệ thống",
+                        isActive: true,
+                        permissions: [] // không set quyền, chỉ cần add ROLE
+                    }
+                ]);
+            }
+
+            if (countUser === 0) {
+                const adminRole = await this.roleModel.findOne({ name: ADMIN_ROLE });
+                const userRole = await this.roleModel.findOne({ name: USER_ROLE })
+                await this.userModel.insertMany([
+                    {
+                        name: "I'm admin",
+                        email: "admin@gmail.com",
+                        password: this.userService.getHashPassword(this.configService.get<string>("INIT_PASSWORD")),
+                        age: 69,
+                        gender: "MALE",
+                        address: "VietNam",
+                        role: adminRole?._id
+                    },
+                    {
+                        name: "I'm Hỏi Dân IT",
+                        email: "quangney@gmail.com",
+                        password: this.userService.getHashPassword(this.configService.get<string>("INIT_PASSWORD")),
+                        age: 96,
+                        gender: "MALE",
+                        address: "VietNam",
+                        role: adminRole?._id
+                    },
+                    {
+                        name: "I'm normal user",
+                        email: "user@gmail.com",
+                        password: this.userService.getHashPassword(this.configService.get<string>("INIT_PASSWORD")),
+                        age: 69,
+                        gender: "MALE",
+                        address: "VietNam",
+                        role: userRole?._id
+                    },
+                ])
+            }
+
+            if (countUser > 0 && countRole > 0 && countPermission > 0) {
+                this.logger.log('>>> ALREADY INIT SAMPLE DATA...');
+            }
+        }
     }
 }
