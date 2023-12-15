b15deb633716b364df6c78fa67c3a2506b3700a9
quangneyy <quangney12602@gmail.com>
2023-12-15 15:43:10 +0700

    upload-CV

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -4,9 +4,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); //key:value
 
-export const RESPONSE_MESSAGE= 'response_message';
+export const RESPONSE_MESSAGE = 'response_message';
 export const ResponseMessage = (message: string) =>
-    SetMetadata(RESPONSE_MESSAGE, message);
+  SetMetadata(RESPONSE_MESSAGE, message);
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
